4a6e9d724690d7b795d7a05f392622a5b16411e4
Fisseha Estifanos <fisseha.137@gmail.com>
2022-10-29 09:47:26 +0300

    added two routes

--- web/app.py
+++ web/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/employee')
+def employee():
+    return render_template('employee.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
